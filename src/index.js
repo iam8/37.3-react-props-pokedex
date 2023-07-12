35468a87c531234c782c79d9a98d82810a897786
alexm <alex-m@outlook.com>
2023-07-12 12:47:03 -0700

    Import ReactDOM; render App component in strict mode

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@
 // 37.3 - React Props & Pokedex
 
 import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
