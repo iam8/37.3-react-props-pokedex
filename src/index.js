fd6840200dcabdc478306dfc780c2d38f6aade97
alexm <alex-m@outlook.com>
2023-07-12 12:34:28 -0700

    Render App component in DOM

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Ioana A Mititean
+// 37.3 - React Props & Pokedex
+
+import React from "react";
+import App from "./App";
+
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
