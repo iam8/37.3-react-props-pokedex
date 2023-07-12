d26d59f251470bd0df68086bfbf77503e1dec2cf
alexm <alex-m@outlook.com>
2023-07-12 12:51:52 -0700

    Defined basic component function

---
+++ src/Pokecard.js
@@ -0,0 +1,16 @@
+// Ioana A Mititean
+// 37.3 - React Props & Pokedex
+
+import React from "react";
+
+
+function Pokecard(props) {
+    return (
+        <>
+
+        </>
+    );
+}
+
+
+export default Pokecard;
---
+++ src/Pokedex.js
@@ -0,0 +1,16 @@
+// Ioana A Mititean
+// 37.3 - React Props & Pokedex
+
+import React from "react";
+
+
+function Pokedex(props) {
+    return (
+        <>
+
+        </>
+    );
+}
+
+
+export default Pokedex;
