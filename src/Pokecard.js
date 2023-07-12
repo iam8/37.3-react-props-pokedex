1f0af096e187265dbced391b827ab80f267d0b7e
alexm <alex-m@outlook.com>
2023-07-12 13:02:50 -0700

    Add Pokemon info to Pokecard component

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -5,10 +5,20 @@ import React from "react";
 
 
 function Pokecard(props) {
-    return (
-        <>
 
-        </>
+    const {id, name, type, base_experience} = props;
+    const imgPath =
+        `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
+    const imgAlt = `${name} Pokemon`;
+
+    return (
+        <div>
+            <h3>{name}</h3>
+            <img src={imgPath} alt={imgAlt}></img>
+            <div>Type: {type}</div>
+            <div>EXP: {base_experience}</div>
+        </div>
     );
 }
 
