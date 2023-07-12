9d12bc0c9f7b62de7705a465b83673ee452a8f48
alexm <alex-m@outlook.com>
2023-07-12 12:27:32 -0700

    Create and export initial App component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// Ioana A Mititean
+// 37.3 - React Props & Pokedex
+
+import React from "react";
+
+
+function App() {
+    return (
+        <div className="App">
+            <p>
+                This is a test paragraph.
+            </p>
+        </div>
+    )
+}
+
+
+export default App;
